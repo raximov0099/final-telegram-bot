3de1c124cb0cd4ba5b829becb6e8da222e94fabc
mashhurbek <mashhurraximov59@gmail.com>
2025-12-04 15:49:06 +0500

    mongoseni uladim

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import "./src/bot/bot.js"
+import mongoose from "mongoose"
+import { config } from "dotenv"
+config()
 
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log(`Db is connected`)
+})
+.catch(() => {
+    console.log(`error: db is not connected`)
+})
 console.log("dastur ishga tushmoqda")
--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,1 +1,16 @@
+import TelegramBot from "node-telegram-bot-api"
+
+import { config } from "dotenv"
+config()
+
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true })
+
+bot.on("message", (msg) => {
+    const chatId = msg.chat.id
+    const first_name = msg.chat.first_name
+    const text = msg.text
+
+    bot.sendMessage(chatId, `assalomu alaykum ${first_name}`);
+    bot.sendMessage(chatId, `${text}`)
+})
 console.log("bot ishga tushdi...")
